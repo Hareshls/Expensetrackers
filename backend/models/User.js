16d573d1f25378bc60a2e277ef8f9fa0c345c483
Hareshls <YOUR_EMAIL@gmail.com>
2026-04-12 23:21:23 +0530

    Clean up User model

--- backend/models/User.js
+++ backend/models/User.js
@@ -11,7 +11,7 @@ const userSchema = new mongoose.Schema({
     salary: { type: Number, default: 0 },
 }, { timestamps: true });
 
-// Hash password before saving
+
 userSchema.pre('save', async function (next) {
     if (!this.isModified('password')) return next();
     this.password = await bcrypt.hash(this.password, 10);
